41795e9fd51d62811f30c0c539a128d8ec06bda5
glnosg <glina.osg@gmail.com>
2017-04-14 15:00:56 +0200

    Fix typos in 'gridLength' and 'gridSize

--- GameHelper.java
+++ GameHelper.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class GameHelper {
 
 	private static final String alphabet = "abcdefg";
-	private int grindLength = 7;
-	private int grindSize = 49;
+	private int gridLength = 7;
+	private int gridSize = 49;
 	private int [] grid = new int[gridSize];
 	private int comCount = 0;
 	
