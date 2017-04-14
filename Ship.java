4bc6bbccb1410d9b551654f8b896ba6358beabf8
glnosg <glina.osg@gmail.com>
2017-04-14 15:11:41 +0200

    Declare String userInput as an argument of checkYourself method

--- Ship.java
+++ Ship.java
@@ -13,10 +13,10 @@ public class Ship {
 		name = n;
 	}
 	
-	public String checkYourself() {
+	public String checkYourself(String userInput) {
 		
 		String result = "miss";
-		int index = locationCells.indexOf(String userInput);
+		int index = locationCells.indexOf(userInput);
 		
 		if (index >= 0)
 			locationCells.remove(index);
